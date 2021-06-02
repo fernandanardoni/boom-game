9bca3c7f8a832ccea36998fd2a8afc8b42e94796
Fernanda Nardoni <thefray.br@gmail.com>
2021-06-01 23:01:14 -0300

    creating score

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const containerBaloes = document.querySelector(".container-baloes");
 
 const startBtn = document.querySelector("button");
 
+const score = document.querySelector("span");
+
+let counter = 0;
+
 function adicionarBalao() {
+
   const elementoImg = document.createElement("img");
 
   elementoImg.setAttribute("src", "./assets/baloon.png");
@@ -18,11 +23,22 @@ function adicionarBalao() {
 
   containerBaloes.appendChild(elementoImg);
 
-  elementoImg.addEventListener("click", function () {
-    elementoImg.remove();
-  
-  });
+  elementoImg.addEventListener("click", () => removeBalloon(elementoImg)); 
 }
+
+
+
+function removeBalloon(element) {
+
+  containerBaloes.removeChild(element);
+  counter = counter + 1;
+  console.log(counter);
+  score.innerText = counter;
+
+}
+
+
+
 
 startBtn.addEventListener("click", function () {
   startBtn.remove();
